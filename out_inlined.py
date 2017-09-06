3e7f907fd3ffbf8997a32953fdfeaf61cb39cdd6
dz0 <jurgis.pralgauskis@gmail.com>
2017-09-06 14:45:50 +0300

    call_map, codes, visited_lines given to  html/js
    todo: inject togglers of inlines

--- out_inlined.py
+++ out_inlined.py
@@ -1,28 +1,32 @@
+
 #@inline 1: __main__. B 
 def B():
-   print("start B")  # line: 283, {}   #module __main__
+           print("start B")
 start B
-   x = 42    # line: 284, {}   #module __main__
-   u = (A(  # line: 285, {'x': 42}   #module __main__
-     3,  # line: 287, {'x': 42}   #module __main__
-      2,  # line: 288, {'x': 42}   #module __main__
-          1   # line: 289, {'x': 42}   #module __main__
-          #@inline 2: __main__. A 
-          def A(*args):
-               a = 5  # line: 275, {'args': (3, 2, 1)}   #module __main__
-               print(  # line: 276, {'args': (3, 2, 1), 'a': 5}   #module __main__
-                 "A"  # line: 277, {'args': (3, 2, 1), 'a': 5}   #module __main__
+           x = 42  
+           u = (A(
+             x=C() ,
+           #@inline 2: __main__. C 
+           def C():
+                    print("C")
+C
+                    return 2
+           #@return: C => 2
+             y=3,
+              z=2
+           #@inline 2: __main__. A 
+           def A(*args, **kwargs):
+                a = 5
+                print(
+                  "A"
 A
-               return a*C()  # line: 279, {'args': (3, 2, 1), 'a': 5}   #module __main__
-               #@inline 3: __main__. C 
-               def C():
-                   print("C")  # line: 296, {}   #module __main__
+                if a > 10:
+                    print("a <= 10")
+a <= 10
+                c = C()
 C
-                   return 2  # line: 297, {}   #module __main__
-               #@return: C => 2
-          #@return: A => 10
-   c = C()  # line: 292, {'x': 42, 'u': 10}   #module __main__
-C
-   print("end B")  # line: 293, {'x': 42, 'u': 10, 'c': 2}   #module __main__
+                return a
+           #@return: A => 5
+           print("end B")
 end B
 #@return: B => None
